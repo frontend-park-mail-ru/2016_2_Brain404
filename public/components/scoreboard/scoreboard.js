721d2ccb0f2cd3fbc2b7c4c5160a55ab782587c9
ekkertigor <ekkert.igor@gmail.com>
2016-10-25 13:57:26 +0300

    Add template for scoreboard

--- public/components/scoreboard/scoreboard.js
+++ public/components/scoreboard/scoreboard.js
@@ -23,68 +23,8 @@
         }
 
         updateHtml(){
-            this.el.innerHTML = `
-                <div class="close_icon close_icon_${this.name}">
-                    <i class="close pink icon float_right"></i>
-                </div>
-                <table class="ui celled padded large table">
-                    <thead>
-                        <tr>
-                          <th><h4 class="ui pink header">Место</h4></th>
-                            <th><h4 class="ui pink header">Логин</h4></th>
-                            <th>
-                                <i class="star pink icon"></i>
-                                <i class="star pink icon"></i>
-                                <i class="star pink half icon"></i>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>
-                              1
-                            </td>
-                            <td>
-                              <h4 class="ui image header">
-                                <div class="content">
-                                  Lena
-                                </div>
-                              </h4></td>
-                            <td>
-                              22
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                              1
-                            </td>
-                            <td>
-                              <h4 class="ui image header">
-                                <div class="content">
-                                  Lena
-                                </div>
-                              </h4></td>
-                            <td>
-                              22
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                              1
-                            </td>
-                            <td>
-                              <h4 class="ui image header">
-                                <div class="content">
-                                  Lena
-                                </div>
-                              </h4></td>
-                            <td>
-                              22
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            `;
+            let _template = window.fest['scoreboard/scoreboard.tmpl'](this);
+            this.el.innerHTML = _template;
         }
     }
 
